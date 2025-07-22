e374a317845e833616ea664e95315d80491d2f40
Tuberoh <165823057+Tuberoh@users.noreply.github.com>
2025-07-22 14:01:30 +0000

    new changes

--- Cuykon Docs/docusaurus.config.js
+++ Cuykon Docs/docusaurus.config.js
@@ -10,7 +10,7 @@ import {themes as prismThemes} from 'prism-react-renderer';
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'About & More',
+  title: 'Cuykon.docx',
   tagline: 'All about the cuykon project',
   favicon: 'img/favicon.ico',
 
